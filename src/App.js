805e65cf564b6b13118b9d2640bbc819ab36f0ed
zrroayb <yamadera1m@gmail.com>
2024-12-28 20:20:15 +0300

    Added new pages and navigation

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
+import About from "./pages/About";
 import "./App.css";
 
 function App() {
   return (
-    <Router basename="/bodrum-nufus">
-      <div className="app-container">
+    <div className="app-container">
+      <Router basename="/bodrum-nufus">
         <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/search" element={<Search />} />
-        </Routes>
-      </div>
-    </Router>
+        <div className="main-content">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
   );
 }
 
