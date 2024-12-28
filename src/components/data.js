1123deaaf9a7e063d3fb40487c416c8773099df2
zrroayb <yamadera1m@gmail.com>
2024-12-28 23:12:58 +0300

    Improved mobile responsiveness

--- src/components/data.js
+++ src/components/data.js
@@ -1,7 +1,7 @@
 export const productData = [
   {
     ProductID: "P001",
-    ProductName: "Laptop",
+    ProductName: "Laptop1",
     Category: "Electronics",
     Owner: "Ayberk Göçer",
     Price: 999.99,
@@ -14,16 +14,22 @@ export const productData = [
     ProductID: "P002",
     ProductName: "Laptop2",
     Category: "Electronics",
+    Owner: "Ayberk Göçer",
     Price: 999.99,
     Stock: 50,
     LastUpdated: "2024-03-20",
+    ImageUrl:
+      "https://media.istockphoto.com/id/1421035376/tr/vektör/realistic-macbook-mockup-blank-white-screen-laptop-vector-template.jpg?s=1024x1024&w=is&k=20&c=jvQ92AMWjb2Hxt2oejtB-wnXuZGXqWMxKfcD-O3vf1U=", // Add image URLs for your products
   },
   {
     ProductID: "P003",
     ProductName: "Laptop3",
     Category: "Electronics",
+    Owner: "Ayberk Göçer",
     Price: 999.99,
     Stock: 50,
     LastUpdated: "2024-03-20",
+    ImageUrl:
+      "https://media.istockphoto.com/id/1421035376/tr/vektör/realistic-macbook-mockup-blank-white-screen-laptop-vector-template.jpg?s=1024x1024&w=is&k=20&c=jvQ92AMWjb2Hxt2oejtB-wnXuZGXqWMxKfcD-O3vf1U=", // Add image URLs for your products
   },
 ];
